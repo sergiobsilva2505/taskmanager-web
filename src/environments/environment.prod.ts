0654026fe6cda2968f6999fd503012ea1d509a30
Sergio Bezerra da Silva <sergio.bezerra@outlook.com>
2026-08-16 13:32:16 -0300

    fix: corrigir env de produção (fileReplacements + googleClientId real)
    * fix: corrigir env de produção (fileReplacements + googleClientId real)
    
    * fix: corrigir env de produção (fileReplacements + googleClientId real)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://taskmanager-api.codefoundry.com.br/api',
-  googleClientId: 'teste123',
+  googleClientId: '770425663420-b6rgcg8h4sgr1qv33hqt1790rf5e1jg6.apps.googleusercontent.com'
 };
+
